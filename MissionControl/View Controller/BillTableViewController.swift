92db937327bfb2e7cef3cc51402eb259ccb9d071
Juan Renteria <jrenteriacamino@outlook.com>
2019-08-01 14:14:43 -0700

    added editing button to BillTableViewController and enabled swipe to delete

--- MissionControl/View Controller/BillTableViewController.swift
+++ MissionControl/View Controller/BillTableViewController.swift
@@ -21,7 +21,7 @@ class BillTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
         
         // load sample bills
         bills = Bill.populateBills()
@@ -50,25 +50,20 @@ class BillTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            bills.remove(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
