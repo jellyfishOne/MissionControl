5fff4762e80c9e6fc1f688e42366589c581ababb
Juan Renteria <jrenteriacamino@outlook.com>
2019-08-01 14:06:43 -0700

    Added "add" bar button to bill List view controller and added it to new navigation controller with modal segue

--- MissionControl/Model/Bill.swift
+++ MissionControl/Model/Bill.swift
@@ -20,7 +20,6 @@ struct Bill {
     static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .short
-        formatter.timeStyle = .short
         return formatter
     }()
     
